f31b66be9034b71040851a30009ce51d282249c4
yash2703 <19ucs256@lnmiit.ac.in>
2020-07-06 19:47:20 +0530

    first

---
+++ index.php
@@ -0,0 +1,8 @@
+<! DOCTYPE>
+<html>
+<body>
+<? php.
+echo "Hello i have successfully completed task2 </br>";
+ECHO "In this task i have integrate multiple tools ,  </br>";
+EcHo " Github , Jenkins , Kubernetes </br>";
+?>
